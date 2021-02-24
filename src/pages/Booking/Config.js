3be2746f619ff494d10e9b6b378490a485b1c31e
marwa <marwajawad19@gmail>
2021-02-25 02:46:03 +0300

    init

--- src/pages/Booking/Config.js
+++ src/pages/Booking/Config.js
@@ -41,7 +41,9 @@ export const BookingColumns = [
     title: "Title",
     dataIndex: "Title",
     render: (item) => (
-      <Link to={`/bookingdetalis/${item.id}`}>{item.title}</Link>
+      <Link to={`/booking`}>{item.title}</Link>
+
+      // <Link to={`/bookingdetalis/${item.id}`}>{item.title}</Link>
     ),
     sorter: {
       compare: (a, b) => a.english - b.english,
